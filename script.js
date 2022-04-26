b8595c03f8f83fa9e392b83f6038b65707a287ac
abol313 <FazlAbol18@gmail.com>
2022-04-26 21:06:23 +0430

    feat: added images

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function enableEn(){
     bd.style.setProperty("--fa-color","cyan")
     bd.style.setProperty("--en-bg-color","cyan")
     bd.style.setProperty("--en-color","#151515")
-    
+    en_spans = document.querySelectorAll('span[lang="en"]');
+    for(let en_span of en_spans)
+        en_span.parentElement.style.direction="ltr";
+
 }
 function enableFa(){
     let bd = document.body
@@ -16,4 +19,8 @@ function enableFa(){
     bd.style.setProperty("--en-color","cyan")
     bd.style.setProperty("--fa-bg-color","cyan")
     bd.style.setProperty("--fa-color","#151515")
+    fa_spans = document.querySelectorAll('span[lang="fa"]');
+    for(let fa_span of fa_spans)
+        fa_span.parentElement.style.direction="rtl";
+
 }
